4b45a106d984a48ed62ad961f58f66b0f2caa658
Jan Nabbefeld <jan.nabbefeld@kreuzwerker.de>
2014-11-19 13:18:56 +0100

    Notification bugfix

--- providers/unit.rb
+++ providers/unit.rb
@@ -4,20 +4,10 @@ def whyrun_supported?
   true
 end
 
+use_inline_resources
+
 action :add do
   Chef::Log.debug "Adding systemd unit file for #{new_resource.name}"
-
-  execute 'systemctl-daemon-reload' do
-    command '/bin/systemctl --system daemon-reload'
-    action :nothing
-  end
-
-  service new_resource.name do
-    action :nothing
-    provider Chef::Provider::Service::Systemd
-    supports :status => true, :restart => true, :reload => true
-    action :nothing
-  end
 
   directory node["systemd"]["servicedir"]["path"] do
     owner node["systemd"]["servicedir"]["owner"]
@@ -55,41 +45,10 @@ action :add do
       :partof=> new_resource.partof,
       :killmode => new_resource.killmode,
     )
-    notifies :run, 'execute[systemctl-daemon-reload]', :immediately
-    if (new_resource.activate)
-      notifies :enable, "service[#{new_resource.name}]", :immediately
-      notifies :restart, "service[#{new_resource.name}]", :immediately
-    end
     cookbook "systemd"
+    action :create
   end
-  new_resource.updated_by_last_action(false)
 end
 
 action :remove do
-  if ::File.exists?("#{node["systemd"]["servicedir"]['path']}/#{new_resource.name}")
-    Chef::Log.debug "Removing #{new_resource.name} from #{node["systemd"]["servicedir"]['path']}"
-
-  execute 'systemctl-daemon-reload' do
-    command '/bin/systemctl --system daemon-reload'
-    action :nothing
-  end
-
-  if (new_resource.activate)
-    service new_resource.name do
-      action :nothing
-      provider Chef::Provider::Service::Systemd
-      supports :status => true, :restart => true, :reload => true
-      action [:disable, :stop]
-    end
-  end
-
-  file "#{node["systemd"]["servicedir"]['path']}/#{new_resource.name}" do
-    action :delete
-    if (new_resource.activate)
-      notifies :run, 'execute[systemctl-daemon-reload]', :immediately
-    end
-  end
-
-    new_resource.updated_by_last_action(true)
-  end
 end
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,17 +7,23 @@
 # All rights reserved - Do Not Redistribute
 #
 
+execute 'systemctl-daemon-reload' do
+  command '/bin/systemctl --system daemon-reload'
+  action :nothing
+end
+
 systemd_unit 'test.service' do
   execstart "/bin/true"
-  execstop "/bin/false"
+  execstop "/bin/true"
   deploypath "/tmp"
+  notifies :run, 'execute[systemctl-daemon-reload]', :immediately
 end
 
-systemd_unit 'test2.service' do
-  execstart "/bin/true"
-  execstop "/bin/false"
-end
+#systemd_unit 'test2.service' do
+#  execstart "/bin/true"
+#  execstop "/bin/false"
+#end
 
-systemd_unit 'test2.service' do
-  action "remove"
-end
+#systemd_unit 'test2.service' do
+#  action "remove"
+#end
